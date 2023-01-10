58b4de9e750732182ccfa6fa70da302612d00662
Vitor-Life <mailto:vitor@lifecti.com.br>
2023-01-09 22:34:06 -0300

    created tkinter

--- shutdown.py
+++ shutdown.py
@@ -1,13 +1,30 @@
 import time
 import pyautogui
+from tkinter import *
+janela = Tk()
+janela.title("Shutdown PC")
+# texto = Label(janela, text="digite quantos minutos você deseja reiniciar o seu pc: ")
+janela.geometry("900x450")
+janela.configure(background="#808080")
+
+
+Label(janela,text="digite aqui",background="#fff",foreground="#009",anchor=W).place(x=10,y=10,width=100,height=20)
+vnome=Entry(janela)
+vnome.place(x=10,y=30,width=200,height=20)
+
+janela.mainloop()
+
+
 
 of = pyautogui
 reiniciar = int(input("digite quantos minutos você deseja reiniciar o seu pc:  "))
 of.hotkey('win', 'r')
 of.typewrite('cmd')
 of.hotkey('enter')
-of.sleep(3)
+of.sleep(1)
 of.typewrite("shutdown -r -t "+str(reiniciar))
-time.sleep(5)
+time.sleep(1)
+of.hotkey('enter')
+of.sleep(3)
 
 
